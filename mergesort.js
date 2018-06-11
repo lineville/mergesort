10306b2c28a513bea32db628067210891ec7a6cb
Graham Johnson <grahamscottjohnson@gmail.com>
2018-06-11 16:12:59 -0400

    passing tests

--- mergesort.js
+++ mergesort.js
@@ -1,15 +1,51 @@
 function split(arr) {
   // splits the array into two arrays
   // right array will take extra element if uneven
+
+  let halfPoint = Math.ceil( arr.length / 2 );
+  let leftArray = arr.slice( 0, halfPoint );
+  let rightArray = arr.slice( halfPoint );
+  return [leftArray, rightArray];
 }
 
-// function merge() {
-//   // merges two sorted arrays into one sorted array
-// }
+function merge( leftArray, rightArray ) { //[2,3], [1]
+  // merges two sorted arrays into one sorted array
 
-// function mergeSort() {
+  let leftPointer = 0;
+  let rightPointer = 0;
+  let result = [];
+  while ( leftPointer < leftArray.length || rightPointer < rightArray.length ){
 
-// }
+    let leftValue = leftArray[leftPointer];
+    let rightValue = rightArray[rightPointer];
+    
+    if ( leftValue < rightValue || !rightValue ){
+      result.push( leftValue );
+      leftPointer += 1;
+    } else {
+      result.push( rightValue );
+      rightPointer += 1;
+    }
+    console.log( result )
+  }
+  return result;
+}
+
+function mergeSort( arrayToBeSorted ) {
+  console.log( arrayToBeSorted );
+  if ( arrayToBeSorted.length <= 1){
+    return arrayToBeSorted;
+  }
+
+  let temp = split( arrayToBeSorted );
+  let leftArray = temp[0];
+  let rightArray = temp[1];
+
+  let sortedLeftArray = mergeSort( leftArray );
+  let sortedRightArray = mergeSort( rightArray );
+
+  return merge( sortedLeftArray, sortedRightArray );
+}
 
 
 // // mergesort scratchpad
