32c4c02e00526a5604dbe92ca552b4a647d4ebd8
Liam Neville <lineville@vassar.edu>
2018-06-11 16:16:49 -0400

    prettified and cleaned

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,54 +1,43 @@
-describe('Split Array function', function() {
-  it('is able to split an array into two halves', function() {
+describe('Split Array function', function () {
+  it('is able to split an array into two halves', function () {
     // your code here
-    let arrayToBeSplit = [1,2,3,4,5];
-    let resultOfSplit = split( arrayToBeSplit );
-    // expect( resultOfSplit )
-        //have a LeftArray
-        //have a RightArray
-        //sizes should differ no more than by 1
-    // [
-    //   leftArray,
-    //   rightArray
-    // ]
-    expect( resultOfSplit.length ).toEqual( 2 );
-    expect( resultOfSplit[0] ).toEqual( [1,2,3] );
-    expect( resultOfSplit[1] ).toEqual( [4,5] );
+    let arrayToBeSplit = [1, 2, 3, 4, 5];
+    let resultOfSplit = split(arrayToBeSplit);
+    expect(resultOfSplit.length).toEqual(2);
+    expect(resultOfSplit[0]).toEqual([1, 2, 3]);
+    expect(resultOfSplit[1]).toEqual([4, 5]);
 
   });
 
-  it( "handles empty array" , function() {
+  it("handles empty array", function () {
     let resultOfSplit = split([]);
-    expect( resultOfSplit[0] ).toEqual( [] );
-    expect( resultOfSplit[1] ).toEqual( [] );
+    expect(resultOfSplit[0]).toEqual([]);
+    expect(resultOfSplit[1]).toEqual([]);
   });
-  it( "handles a singleton array", function(){
+  it("handles a singleton array", function () {
     let resultOfSplit = split([1]);
-    expect( resultOfSplit[0] ).toEqual( [1] );
-    expect( resultOfSplit[1] ).toEqual( [] );
+    expect(resultOfSplit[0]).toEqual([1]);
+    expect(resultOfSplit[1]).toEqual([]);
   });
 });
 
-describe('Merge function', function(){
-  it('is able to merge two sorted arrays into one sorted array', function(){
+describe('Merge function', function () {
+  it('is able to merge two sorted arrays into one sorted array', function () {
     // test the merging algorithm
-    let firstSortedArray = [1,2,3,4,5,6];
+    let firstSortedArray = [1, 2, 3, 4, 5, 6];
     let secondSortedArray = [1, 3, 5, 7];
-    expect( merge( firstSortedArray, secondSortedArray ) ).toEqual( [1,1,2,3,3,4,5,5,6,7] );
+    expect(merge(firstSortedArray, secondSortedArray)).toEqual([1, 1, 2, 3, 3, 4, 5, 5, 6, 7]);
   });
-
-  //handles a []
 });
 
-describe("MergeSort function", function(){
-
-  it( "sorts the array", function(){
-    let array = [4,2,7,8,1];
-    expect( mergeSort(array) ).toEqual( [1,2,4,7,8] ); 
+describe("MergeSort function", function () {
+  it("sorts the array", function () {
+    let array = [4, 2, 7, 8, 1];
+    expect(mergeSort(array)).toEqual([1, 2, 4, 7, 8]);
   })
-  it( "sorts another array", function(){
-    let array = [2,3,1];
-    expect( mergeSort(array) ).toEqual( [1,2,3] ); 
+  it("sorts another array", function () {
+    let array = [2, 3, 1];
+    expect(mergeSort(array)).toEqual([1, 2, 3]);
   })
 });
 
