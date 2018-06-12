375e2381a69fc3319a3b248668f9e920bf130d37
Liam Neville <lineville@vassar.edu>
2018-06-11 21:10:21 -0400

    comments added to clarify

--- mergesort.js
+++ mergesort.js
@@ -10,23 +10,26 @@ function split(arr) {
 
 function merge(leftArray, rightArray, comparator) { //[2,3], [1]
   // merges two sorted arrays into one sorted array
+  // if no comparator was provided assign the default comparison
   if (!comparator){
     comparator = (a, b) => {
       return a < b;
-    }
+    };
   }
 
-  let leftPointer = 0;
-  let rightPointer = 0;
-  let result = [];
+  let leftPointer = 0; // points to current element in left array
+  let rightPointer = 0; // points to current element in right array
+  let result = []; // resulting merged array
+  // while we still have remaining elements in the left or right
   while (leftPointer < leftArray.length || rightPointer < rightArray.length) {
-
+    // get the values at left and right pointers
     let leftValue = leftArray[leftPointer];
     let rightValue = rightArray[rightPointer];
+    // if there is a left value and the left is smaller or there is no right value then push the left and move pointer
     if ( leftValue && ( !rightValue || comparator(leftValue, rightValue) ) ) {
       result.push(leftValue);
       leftPointer += 1;
-    } else {
+    } else { // otherwise push the right value and move pointer
       result.push(rightValue);
       rightPointer += 1;
     }
@@ -35,17 +38,17 @@ function merge(leftArray, rightArray, comparator) { //[2,3], [1]
 }
 
 function mergeSort(arrayToBeSorted, comparator ) {
-
+  // Base Case: we have split into arrays of length 1
   if (arrayToBeSorted.length <= 1) {
     return arrayToBeSorted;
   }
-
+  // split the array into two arrays using split
   let temp = split(arrayToBeSorted);
   let leftArray = temp[0];
   let rightArray = temp[1];
-
+  // mergeSort recursively on left and right arrays
   let sortedLeftArray = mergeSort(leftArray);
   let sortedRightArray = mergeSort(rightArray);
-
+  // return the merge of the two sorted arrays using comparator
   return merge(sortedLeftArray, sortedRightArray, comparator);
 }
